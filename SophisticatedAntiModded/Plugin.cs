6d78dd6d0e3c9eddd029f4fc49f3c5aeeb8f3948
NgbatzStudios <95753063+maskedVR@users.noreply.github.com>
2024-11-06 10:54:15 +1100

    Add files via upload

---
+++ SophisticatedAntiModded/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using BepInEx;
+using UnityEngine;
+using Newtilla;
+
+namespace SophisticatedAntiModded
+{
+	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		bool inRoom;
+
+		void Start()
+		{
+			inRoom = false;
+		}
+
+		void OnEnable()
+		{
+			HarmonyPatches.ApplyHarmonyPatches();
+		}
+
+		void OnDisable()
+		{
+			HarmonyPatches.RemoveHarmonyPatches();
+		}
+		void Update()
+		{
+			if(inRoom)
+			{
+				Application.Quit();
+			}
+		}
+		public void OnModdedJoin(string gamemode)
+		{
+			inRoom = true;
+		}
+	}
+}
